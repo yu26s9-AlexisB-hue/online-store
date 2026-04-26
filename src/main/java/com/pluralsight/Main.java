8785c9a3faec2714c792d47df46bbadf7cb9cc03
Alexis Barrett <happynessb09@gmail.com>
2026-04-25 20:02:35 -0400

    getting my list to print out

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,49 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args){
+        ArrayList<Product> inventory;
+        inventory = getInventory();
+
+        //System.out.println("The current list: ");
+
+        for(int i = 0; i < inventory.size(); i++){
+            Product n = inventory.get(i);
+
+            System.out.printf("SKU: %s %s - Price:$%.2f %s\n ", n.getSku(), n.getName(), n.getPrice(), n.getDepartment());
+        }
+
+
+    }
+    private static ArrayList<Product> getInventory(){
+        ArrayList<Product> result = new ArrayList<>();
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("products.csv"));
+
+            String line;
+
+            //skip the header
+            reader.readLine();
+
+
+            //creating a loop that reads line by line.
+            while((line = reader.readLine()) != null ){
+
+                Product p = new Product(line);
+
+                // Adds information to the Array list.
+                result.add(p);
+            }
+            reader.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Error: "+ e.getMessage());
+        }
+        return result;
+    }
 }
